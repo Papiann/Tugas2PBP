461c5d03e430a4859b66d757382c9dbae91edba6
Rafi Rasendrya Favian <rafi.rasendrya@ui.ac.id>
2022-09-09 20:26:41 +0700

    Bismillah Deploy Tugas2PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_catalog': data_barang_katalog,
+    'nama': 'Rafi Rasendrya Favian',
+    'npm' : '2106751581'
+}
+
+    return render(request, "katalog.html", context)
