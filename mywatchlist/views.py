9622b66df68c145faebc51ddc40873f87b0107bd
Rafi Rasendrya Favian <rafi.rasendrya@ui.ac.id>
2022-09-21 15:00:50 +0700

    Routing XML and JSON

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_mywatchlist(request):
@@ -11,3 +13,25 @@ def show_mywatchlist(request):
 }
 
     return render(request, "mywatchlist.html", context)
+
+def show_xml(request):
+    data = MyWatchList.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MyWatchList.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+
+    # Jika JSON
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MyWatchList.objects.filter(pk=id)
+
+    # Jika XML
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
