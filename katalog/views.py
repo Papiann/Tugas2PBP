1b4db31cc9e4f303f2e64739ed0cc31a32b8022c
Rafi Rasendrya Favian <rafi.rasendrya@ui.ac.id>
2022-09-21 20:16:30 +0700

    Finish Testing HTML JSON XML

--- katalog/views.py
+++ katalog/views.py
@@ -11,25 +11,3 @@ def show_katalog(request):
 }
 
     return render(request, "katalog.html", context)
-
-def show_xml(request):
-    data = BarangWishlist.objects.all()
-
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-
-def show_json(request):
-    data = BarangWishlist.objects.all()
-
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_json_by_id(request, id):
-    data = BarangWishlist.objects.filter(pk=id)
-
-    # Jika JSON
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-
-def show_xml_by_id(request, id):
-    data = BarangWishlist.objects.filter(pk=id)
-
-    # Jika XML
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
---
+++ mywatchlist/README.md
@@ -0,0 +1,19 @@
+# Tugas 3: Pengimplementasian Data Delivery Menggunakan Django
+
+Pemrograman Berbasis Platform (CSGE602022) - diselenggarakan oleh Fakultas Ilmu Komputer Universitas Indonesia, Semester Ganjil 2022/2023 
+
+----------------------------------------------------
+
+### Link Heroku
+pbp-tugas2-papian.herokuapp.com/mywatchlist
+
+----------------------------------------------------
+
+### Jelaskan perbedaan antara JSON, XML, dan HTML!
+Jawab :
+
+### Jelaskan mengapa kita memerlukan data delivery dalam pengimplementasian sebuah platform?
+Jawab :
+
+### Jelaskan bagaimana cara kamu mengimplementasikan checklist di atas.
+Jawab :
